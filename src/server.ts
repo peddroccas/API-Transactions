4f2a2481f62c934f176f60bf7933767809cf3c9d
Pedro Antunes <pedro@ctir.gov.br>
2024-08-13 15:59:52 -0300

    feat: add env validation

--- src/server.ts
+++ src/server.ts
@@ -4,9 +4,8 @@ import { knex } from './database'
 const app = fastify()
 
 app.get('/hello', async () => {
-  const tables = await knex('sqlite_schema').select('*')
-
-  return tables
+  const transaction = await knex('transactions').select('*')
+  return transaction
 })
 
 app.listen({ port: 3333 }).then(() => {
